48fe7e7f0a168584945da7e47bcbac19b90627f4
白倉　稔浩 <s561233@shirakura-toshihiro.local>
2016-12-16 03:43:22 +0900

    no message

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,1 +1,34 @@
-console.log('\'Allo \'Allo!');
+$(function(){
+  //******************************************
+  // 基本情報の詳細ボタン押下時
+  //******************************************
+  $('.link__disp-detail--basic').on('tap',function(){
+    var self = $(this)
+    var idElem = $(this).parents('.card'); 
+    idElem.find('.card__detail-wrapper').toggle(100,function(){
+      if($(this).css('display') == 'block'){
+        self.html('詳細情報を閉じる');
+        idElem.find('.card__flow').css({"margin-top": "0px","border-radius": "10px"});
+      }else{
+        self.html('詳細情報を開く');
+        idElem.find('.card__flow').css({"margin-top": "-10px","border-radius": "0 0 10px 10px"});
+      }
+    });
+  });
+  
+  
+  //******************************************
+  // 詳細情報の詳細ボタン押下時
+  //******************************************
+  $('.link__disp-detail--option').on('tap',function(){
+    var self = $(this)
+    var targetElem = $(this).next(); 
+    targetElem.toggle(100,function(){
+      if($(this).css('display') == 'block'){
+        self.html('詳細情報を閉じる');
+      }else{
+        self.html('詳細情報を開く');
+      }
+    });
+  });
+});
